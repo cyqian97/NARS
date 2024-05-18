6461d2a5644c4f9cdeea73f1be9560d410924922
cyqian97 <62449732+cyqian97@users.noreply.github.com>
2024-05-18 17:55:40 -0500

    writing exist conv path

--- matching/definitions.py
+++ matching/definitions.py
@@ -1,33 +1,46 @@
+NODE_NUM = 0 # the total number of nodes
+
 class Node():
     __slots__ = ("id", "num_p", "num_n")
 
     def __init__(self, id, num_p, num_n):
+        """Node class fror matching problem
+
+        Args:
+            id (int): node id, start from 1
+            num_p (int): number of positive connectors
+            num_n (int): number of negative connectors
+        """
         self.id = id
         self.num_p = num_p
         self.num_n = num_n
+        NODE_NUM+=1
 
-    def __getitem__(self, side): 
+    def __getitem__(self, side):
         if side == 1:
             return self.num_p
         elif side == -1:
             return self.num_n
         else:
-            raise Exception(f"__getitem__: ERROR: side must be +1 or -1, but is {side}")
-  
+            raise Exception(
+                f"__getitem__: ERROR: side must be +1 or -1, but is {side}")
+
     def connected(side):
         if side == 1:
             if self.num_p > 0:
                 self.num_p -= 1
             else:
-                raise Exception(f"connected: ERROR: no more positive connectors")
+                raise Exception(
+                    f"connected: ERROR: no more positive connectors")
         elif side == -1:
             if self.num_n > 0:
                 self.num_n -= 1
             else:
-                raise Exception(f"connected: ERROR: no more negative connectors")
+                raise Exception(
+                    f"connected: ERROR: no more negative connectors")
         else:
-            raise Exception(f"__getitem__: ERROR: side must be +1 or -1, but is {side}")
-
+            raise Exception(
+                f"__getitem__: ERROR: side must be +1 or -1, but is {side}")
 
 
 class Edge():
@@ -36,8 +49,6 @@ class Edge():
         self.n2 = n2
         self.side1 = side1
         self.side2 = side2
-        n1.connected(side1)
-        n2.connected(side2)
 
     def __eq__(self, edge):
         if self.n1 == edge.n1 and self.n2 == edge.n2 and self.side1 == edge.side1 and self.side2 == edge.side2:
@@ -50,16 +61,19 @@ class Edge():
     def __hash__(self):
         return hash((self.n1, self.side1)) ^ hash((self.n2, self.side2))
 
+
 def can_connect(n1, side1, n2, side2):
-    if n1[side1]>0 and n2[side2]>0:
+    if n1[side1] > 0 and n2[side2] > 0:
         return True
     else:
         return False
 
-def connect(n1, side1, n2, side2):
-    if n1[side1]>0 and n2[side2]>0:
-        return Edge(n1,side1,n2,side2)
 
+def connect(n1, side1, n2, side2):
+    if n1[side1] > 0 and n2[side2] > 0:
+        n1.connected(side1)
+        n2.connected(side2)
+        return Edge(n1, side1, n2, side2)
 
 
 def is_cw(n1, n2, n3):
@@ -71,3 +85,25 @@ def is_cw(n1, n2, n3):
     return ((n1 <= n2 and n2 < n3) or (n1 < n2 and n2 <= n3) or
             (n2 <= n3 and n3 < n1) or (n2 < n3 and n3 <= n1) or
             (n3 <= n1 and n1 < n2) or (n3 < n1 and n1 <= n2))
+
+
+def next_id(node):
+    """Get the next node id in the clockwise direction
+
+    Args:
+        node (Node): current node
+        NODE_NUM (int): the total number of nodes
+    """
+    return ((node.id - 1) % NODE_NUM)+1
+
+
+def exist_convex_path(n_p, n_n, NODE_NUM):
+    """Check it there is a convex path going clockwise from n1 to n2
+
+    Args:
+        n1 (Node): 
+        n2 (Node): 
+        NODE_NUM (int): the total number of nodes
+    """
+    next_id = n1.id
+    while 
